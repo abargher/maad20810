cdec6f60ce9a66f88568c9b4c60eed8323880b15
Alec Bargher <abargher@uchicago.edu>
2024-03-27 15:12:08 -0400

    alec: image generation

--- sketch1/gen_img.py
+++ sketch1/gen_img.py
@@ -1,1 +1,30 @@
+from typing import Callable
 from PIL import Image
+
+IMG_HEIGHT = 256
+
+# borrowed from
+# https://stackoverflow.com/a/58382956
+def gen_pixel(x: int, y: int) -> tuple[int, int, int]:
+    p = (x % 255,
+         y % 255,
+         (x**2-y**2) % 255)
+    return p
+
+def generate_img(width: int,
+                 pgen:Callable[[int, int], tuple[int,int,int]],
+                 path: str):
+    img = Image.new('RGB', (IMG_HEIGHT, width), 255)
+    data = img.load()
+    for x in range(img.size[0]):
+        for y in range(img.size[1]):
+            data[x,y] = pgen(x, y)
+
+    img.save(path)
+
+def main():
+    generate_img(256, gen_pixel, "test_img1.png")
+    return
+
+if __name__ == "__main__":
+    main()
